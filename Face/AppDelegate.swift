de8cd4e7221bb69d5f748051f5d8888f37a56261
zengzhaoyang <664587718@qq.com>
2016-04-09 17:36:01 +0800

    inti

--- Face/AppDelegate.swift
+++ Face/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.backgroundColor = UIColor.whiteColor()
+        self.window?.makeKeyAndVisible()
+        let rootController = ViewController()
+        self.window?.rootViewController = rootController
         // Override point for customization after application launch.
         return true
     }
--- Face/ViewController.swift
+++ Face/ViewController.swift
@@ -7,17 +7,120 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+    let captureSession = AVCaptureSession()
+    var captureDevice: AVCaptureDevice?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    var headImage: UIView?
+    
+    let kScreenWidth = UIScreen.mainScreen().bounds.width
+    let kScreenHeight = UIScreen.mainScreen().bounds.height
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        headImage = UIView(frame: CGRectMake(0, 0, 100, 100))
+        headImage?.backgroundColor = UIColor.clearColor()
+        headImage?.layer.borderColor = UIColor.redColor().CGColor
+        headImage?.layer.borderWidth = 3
+        headImage?.layer.zPosition = 1000
+        self.view.addSubview(headImage!)
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetLow
+        let devices = AVCaptureDevice.devices()
+        for device in devices {
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                if (device.position == AVCaptureDevicePosition.Front) {
+                    captureDevice = device as? AVCaptureDevice
+                    if captureDevice != nil {
+                        print("Capture Device found")
+                        beginSession()
+                    }
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func beginSession() {
+        
+        try! captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+        let output = AVCaptureVideoDataOutput()
+        
+        let cameraQueue = dispatch_queue_create("cameraQueue", DISPATCH_QUEUE_SERIAL)
+        output.setSampleBufferDelegate(self, queue: cameraQueue)
+        let value: NSNumber = NSNumber(unsignedInt: kCVPixelFormatType_32BGRA)
+        output.videoSettings = [kCVPixelBufferPixelFormatTypeKey: value]
+        captureSession.addOutput(output)
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        previewLayer?.frame = self.view.bounds
+        self.view.layer.addSublayer(previewLayer!)
+        captureSession.startRunning()
+        
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
+        let resultImage = sampleBufferToImage(sampleBuffer)
+        let context = CIContext(options: [kCIContextUseSoftwareRenderer: true])
+        let detecotr = CIDetector(ofType:CIDetectorTypeFace,  context:context, options:[CIDetectorAccuracy: CIDetectorAccuracyHigh])
+        
+        let ciImage = CIImage(image: resultImage)
+        let results:NSArray = detecotr.featuresInImage(ciImage!, options: ["CIDetectorImageOrientation" : 6])
+        let imageSize = resultImage.size
+        let offsetx = (imageSize.width / imageSize.height * kScreenHeight - kScreenWidth) / 2
+        if results.count == 0 {
+            dispatch_async(dispatch_get_main_queue(), {
+                self.headImage?.hidden = true
+            })
+        }
+        for r in results {
+            let face: CIFaceFeature = r as! CIFaceFeature
+//            print(face.bounds.origin.x, face.bounds.origin.y, face.bounds.size.width, face.bounds.size.height)
+            
+            let width = face.bounds.size.width / imageSize.height * kScreenHeight
+            let height = face.bounds.size.height / imageSize.height * kScreenHeight
+            let x = kScreenWidth - (face.bounds.origin.y / imageSize.height * kScreenHeight - offsetx) - width
+            let y = face.bounds.origin.x / imageSize.height * kScreenHeight
+            
+//            print(x, y, width, height)
+            dispatch_async(dispatch_get_main_queue(), {
+                self.headImage?.hidden = false
+                self.headImage?.frame = CGRectMake(x, y, width, height)
+            })
+            //headImage?.frame = face.bounds
+        }
+    }
+    
+    func sampleBufferToImage(sampleBuffer: CMSampleBuffer) -> UIImage {
+        let imageBuffer: CVImageBufferRef = CMSampleBufferGetImageBuffer(sampleBuffer)!
+        CVPixelBufferLockBaseAddress(imageBuffer, 0)
+        let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer)
+        
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+        let width = CVPixelBufferGetWidth(imageBuffer)
+        let height = CVPixelBufferGetHeight(imageBuffer)
+        
+        let colorSpace: CGColorSpaceRef = CGColorSpaceCreateDeviceRGB()!
+        
+        let bitsPerCompornent = 8
+        let bitmapInfo = CGBitmapInfo(rawValue: (CGBitmapInfo.ByteOrder32Little.rawValue | CGImageAlphaInfo.PremultipliedFirst.rawValue) as UInt32)
+        
+        let newContext = CGBitmapContextCreate(baseAddress, width, height, bitsPerCompornent, bytesPerRow, colorSpace, bitmapInfo.rawValue)! as CGContextRef
+        
+        let imageRef: CGImageRef = CGBitmapContextCreateImage(newContext)!
+        let resultImage = UIImage(CGImage: imageRef, scale: 1.0, orientation: UIImageOrientation.Right)
+        
+        return resultImage
     }
 
 
